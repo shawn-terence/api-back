ef9634a5951f45ccd8480ede8618b1da78698367
Dave <daveroymutisya2@gmail.com>
2024-04-16 15:59:43 +0300

    Completed Models

---
+++ models.py
@@ -0,0 +1,74 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flickfusion.db' 
+
+db = SQLAlchemy(app)
+
+# Define the association table for the many-to-many relationship between User and Movie
+user_movie_association = db.Table('user_movie_association',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True)
+)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    gmail = db.Column(db.String)
+    password = db.Column(db.String)
+
+    # Define the relationship with Movie
+    favorite_movies = db.relationship('Movie', secondary=user_movie_association, backref='fans')
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+            'gmail': self.gmail,
+            'password': self.password
+        }
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    year = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    rating = db.Column(db.Float)
+    poster = db.Column(db.String(255))
+    trailer_url = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(50))
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'title': self.title,
+            'year': self.year,
+            'description': self.description,
+            'rating': self.rating,
+            'poster': self.poster,
+            'trailer_url': self.trailer_url,
+            'genre': self.genre
+        }
+
+class Nowshowing(db.Model):
+    __tablename__ = 'Nowshowing'
+
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
+    runtime = db.Column(db.Integer)
+
+    # Define the one-to-one relationship with Movie
+    movie = db.relationship('Movie', backref='nowshowing')
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'movie_id': self.movie_id,
+            'runtime': self.runtime,
+            # You can include other attributes from Movie if needed
+        }
